877f435687c82307b2fb68994166af197e136292
jsb3182 <jsbaq8231516@naver.com>
2026-01-30 23:01:03 +0900

    add 13

--- javatest/untitled/src/testcat.java
+++ javatest/untitled/src/testcat.java
@@ -1,6 +1,8 @@
 public class testcat {
     public static void main(String[] args){
-        System.out.println("\\  /\\");
-        System.out.println()
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
     }
 }
